7c616defde36ae40158b4f139a6c671028027e82
Phi Desktop <phinguyen98.work@gmail.com>
2024-05-27 00:48:19 +0700

    working on ingestion pipeline: desktop development

--- scripts/07_generate_multiple_date_sales_data_with_load.py
+++ scripts/07_generate_multiple_date_sales_data_with_load.py
@@ -4,22 +4,21 @@ from src.generator.generator import DataGenerator
 import time
 from datetime import datetime, timedelta
 
-def main(n_each_day: int, start_date: str, end_date: str, n_processes: int = 8):
+def main(n_each_day: int, start_date: str, end_date: str):
     conn = DBConnection()
     generator = DataGenerator(conn)
-    generator.generate_and_load(n_each_day, start_date, end_date, n_processes)
+    generator.generate_and_load(n_each_day, start_date, end_date)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate sales data.')
-    parser.add_argument('--n_each_day', type=int, required=True, help='Number of sales orders to be created.')
-    parser.add_argument('--start_date', type=str, required=True, help='Date to generate data, format as YYYY-MM-DD')
-    parser.add_argument('--end_date', type=str, required=True, help='Date to generate data, format as YYYY-MM-DD')
-    parser.add_argument('--n_processes', type=int, required=False, default=8, help='Number of processes to generate data')
+    parser.add_argument('--n_each_day', '-n', type=int, required=True, help='Number of sales orders to be created.')
+    parser.add_argument('--start_date', '-sdate', type=str, required=True, help='Date to generate data, format as YYYY-MM-DD')
+    parser.add_argument('--end_date', '-edate', type=str, required=True, help='Date to generate data, format as YYYY-MM-DD')
     
     args = parser.parse_args()
     
     start_time = time.time()
-    main(args.n_each_day, args.start_date, args.end_date, args.n_processes)
+    main(args.n_each_day, args.start_date, args.end_date)
     end_time = time.time()
     
     execution_time = end_time - start_time
--- src/generator/generator.py
+++ src/generator/generator.py
@@ -9,6 +9,10 @@ from faker import Faker
 from typing import List, Dict
 import csv
 from datetime import datetime, timedelta
+from tqdm import tqdm
+from queue import Queue
+import threading
+import os
 
 class DataGenerator:
     def __init__(self, conn: DBConnection) -> None:
@@ -36,130 +40,127 @@ class DataGenerator:
         finally:
             self.conn.close()  
     
-    def generate_sales_data_process(
-        self,
-        n_orders_per_process: int,
-        order_date: str,
-        max_customer_id_shared,
-        start_sales_order_id: int,
-        lock,
-        process_id: int,
-        result_queue
-    ) -> None:      
-        fake = Faker()
-        customer_data = []
-        sales_order_header_data = []
-        sales_order_detail_data = []
-
-        for i in range(n_orders_per_process):
-            if randint(1, 10) > 8:
-                lock.acquire()
-                max_customer_id_shared.value += 1
-                curr_customer_id = max_customer_id_shared.value
-                lock.release()
-                new_customer = (
-                    curr_customer_id,
-                    fake.name(),
-                    fake.email(),
-                    randint(18, 90),
-                    order_date
-                )
-                customer_data.append(new_customer)
-            else:
-                curr_customer_id = randint(1, max_customer_id_shared.value)
-
-            new_sales_order_header = (
-                start_sales_order_id + i,
-                order_date,
-                curr_customer_id
-            )
-            sales_order_header_data.append(new_sales_order_header)
-
-            n_lines = randint(1, 10)
-            for line_number in range(n_lines):
-                new_sales_order_detail = (
-                    start_sales_order_id + i,
-                    line_number + 1,
-                    randint(1, 606),
-                    randint(1, 20),
-                    order_date
-                )
-                sales_order_detail_data.append(new_sales_order_detail)
-
-        result_queue.put((process_id, customer_data, sales_order_header_data, sales_order_detail_data))
-        
-    def save_to_csv(self, order_date: str, result_queue, n_processes: int) -> List[str]:
-        print("Start writing data to .csv files")
-        customers_data = []
-        all_sales_order_header_data = []
-        all_sales_order_detail_data = []
-
-        for _ in range(n_processes):
-            process_id, customer_data, sales_order_header_data, sales_order_detail_data = result_queue.get()
-            customers_data.extend(customer_data)
-            all_sales_order_header_data.extend(sales_order_header_data)
-            all_sales_order_detail_data.extend(sales_order_detail_data)
-
-        customers_df = pd.DataFrame(customers_data, columns=["CustomerID", "Name", "Email", "Age", "ModifiedDate"])
-        customers_df.sort_values(by='CustomerID', ascending=True, inplace=True)
-        sales_order_header_df = pd.DataFrame(all_sales_order_header_data, columns=["SalesOrderID", "OrderDate", "CustomerID"])
-        sales_order_header_df.sort_values(by='SalesOrderID', ascending=True, inplace=True)
-        sales_order_detail_df = pd.DataFrame(all_sales_order_detail_data, columns=["SalesOrderID", "SalesOrderLineNumber", "ProductKey", "Qty", "ModifiedDate"])
-        sales_order_detail_df.sort_values(by=['SalesOrderID', 'SalesOrderLineNumber'], ascending=[True, True], inplace=True)
-
-        customder_csv_file_path = "/tmp/Customer_" + order_date + ".csv"
-        salesorderheader_csv_file_path = "/tmp/SalesOrderHeader_" + order_date + ".csv"
-        salesorderdetail_csv_file_path = "/tmp/SalesOrderDetail_" + order_date + ".csv"
-        
-        customers_df.to_csv(customder_csv_file_path, index=False, sep=',', quotechar='"', lineterminator='\n', quoting=csv.QUOTE_ALL)
-        sales_order_header_df.to_csv(salesorderheader_csv_file_path, index=False, sep=',', quotechar='"', lineterminator='\n', quoting=csv.QUOTE_ALL)
-        sales_order_detail_df.to_csv(salesorderdetail_csv_file_path, index=False, sep=',', quotechar='"', lineterminator='\n', quoting=csv.QUOTE_ALL)
-        print("Finish writing data to .csv files")
-        return [customder_csv_file_path, salesorderheader_csv_file_path, salesorderdetail_csv_file_path]
-    
-    def generate_1_day_sales_data(
-        self,
-        n_orders: int, 
-        order_date: str,
-        n_processes: int
-    ) -> Dict:
-        
-        # Get current max_customer_id and max_sales_order_id from mysql database
+    def generate_data(self, n_orders: int, order_date: str, batch_size: int, queue: Queue) -> None:
+        print(f"Generator Process ID: {os.getpid()}")
         try:
             max_customer_id = self.conn.execute_query("SELECT MAX(CustomerId) FROM Customer")[0][0]
+            if not max_customer_id:
+                max_customer_id = 0
             max_sales_order_id = self.conn.execute_query("SELECT MAX(SalesOrderId) FROM SalesOrderHeader")[0][0]
+            if not max_sales_order_id:
+                max_sales_order_id = 0
         except Exception as e:
             raise Exception(e)
-        
-        n_orders_per_process = n_orders // n_processes
-        remainder_orders = n_orders % n_processes
-        max_customer_id_shared = Value('i', max_customer_id)
-        manager = Manager()
-        result_queue = manager.Queue()
-        processes = []
-        lock = Lock()
-        
-        for i in range(n_processes):
-            additional_order = 1 if i < remainder_orders else 0
-            start_sales_order_id = max_sales_order_id + i * (n_orders_per_process + additional_order)
-            args = (n_orders_per_process + additional_order, order_date, max_customer_id_shared, start_sales_order_id, lock, i, result_queue)
-            p = Process(target=self.generate_sales_data_process, args=args)
-            p.start()
-            processes.append(p)
-        
-        for p in processes:
-            p.join()
-        
-        file_paths = self.save_to_csv(order_date=order_date, result_queue=result_queue, n_processes=n_processes)
-        table_names = ["Customer", "SalesOrderHeader", "SalesOrderDetail"]
-        return dict(zip(table_names, file_paths))
+
+        fake = Faker()
+
+        for batch_start in tqdm(range(0, n_orders, batch_size), desc="Generating orders in batches"):
+            customer_data = []
+            sales_order_header_data = []
+            sales_order_detail_data = []
+
+            batch_end = min(batch_start + batch_size, n_orders)
+            for _ in range(batch_start, batch_end):
+                if randint(1, 10) > 8:
+                    max_customer_id += 1
+                    curr_customer_id = max_customer_id
+                    new_customer = (
+                        curr_customer_id,
+                        fake.name(),
+                        fake.email(),
+                        randint(18, 90),
+                        order_date
+                    )
+                    customer_data.append(new_customer)
+                else:
+                    curr_customer_id = randint(1, max_customer_id)
+
+                max_sales_order_id += 1
+                new_sales_order_header = (
+                    max_sales_order_id,
+                    order_date,
+                    curr_customer_id
+                )
+                sales_order_header_data.append(new_sales_order_header)
+
+                n_lines = randint(1, 10)
+                for line_number in range(n_lines):
+                    new_sales_order_detail = (
+                        max_sales_order_id,
+                        line_number + 1,
+                        randint(1, 606),
+                        randint(1, 20),
+                        order_date
+                    )
+                    sales_order_detail_data.append(new_sales_order_detail)
+
+            queue.put((customer_data, sales_order_header_data, sales_order_detail_data))
+
+        queue.put(None)  # Signal that generation is done
+
+    def write_data_to_csv(self, order_date: str, queue: Queue) -> Dict:
+        print(f"Writer Process ID: {os.getpid()}")
+        customer_csv_file_path = f"/tmp/Customer_{order_date}.csv"
+        salesorderheader_csv_file_path = f"/tmp/SalesOrderHeader_{order_date}.csv"
+        salesorderdetail_csv_file_path = f"/tmp/SalesOrderDetail_{order_date}.csv"
+
+        with open(customer_csv_file_path, mode='w', newline='\n') as customer_file, \
+             open(salesorderheader_csv_file_path, mode='w', newline='\n') as salesorderheader_file, \
+             open(salesorderdetail_csv_file_path, mode='w', newline='\n') as salesorderdetail_file:
+            
+            customer_writer = csv.writer(customer_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, lineterminator="\n")
+            salesorderheader_writer = csv.writer(salesorderheader_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, lineterminator="\n")
+            salesorderdetail_writer = csv.writer(salesorderdetail_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, lineterminator="\n")
+
+            # Write headers
+            customer_writer.writerow(["CustomerID", "Name", "Email", "Age", "ModifiedDate"])
+            salesorderheader_writer.writerow(["SalesOrderID", "OrderDate", "CustomerID"])
+            salesorderdetail_writer.writerow(["SalesOrderID", "SalesOrderLineNumber", "ProductKey", "Qty", "ModifiedDate"])
+
+            while True:
+                data = queue.get()
+                if data is None:
+                    break  # Exit signal
+
+                customer_data, sales_order_header_data, sales_order_detail_data = data
+
+                customer_writer.writerows(customer_data)
+                salesorderheader_writer.writerows(sales_order_header_data)
+                salesorderdetail_writer.writerows(sales_order_detail_data)
+
+        print("Finish writing data to .csv files")
+
+        return {
+            "customer_csv_file_path": customer_csv_file_path,
+            "salesorderheader_csv_file_path": salesorderheader_csv_file_path,
+            "salesorderdetail_csv_file_path": salesorderdetail_csv_file_path
+        }
+
+    def generate_1_day_sales_data(self, n_orders: int, order_date: str) -> Dict:
+        print(f"Main Process ID: {os.getpid()}")
+        batch_size = 100000
+        queue = Queue(maxsize=10)  # Limit the queue size to control memory usage
+
+        generator_thread = threading.Thread(target=self.generate_data, args=(n_orders, order_date, batch_size, queue), name="GeneratorThread")
+        writer_thread = threading.Thread(target=self.write_data_to_csv, args=(order_date, queue), name="WriterThread")
+
+        generator_thread.start()
+        writer_thread.start()
+
+        generator_thread.join()
+        writer_thread.join()
+
+        return {
+            "Customer": f"/tmp/Customer_{order_date}.csv",
+            "SalesOrderHeader": f"/tmp/SalesOrderHeader_{order_date}.csv",
+            "SalesOrderDetail": f"/tmp/SalesOrderDetail_{order_date}.csv"
+        }
     
     def generate_and_load(
         self,
         n_orders: int, 
         start_date: str,
-        end_date: str,
-        n_processes: int
+        end_date: str
     ) -> None:
         try:
             self.conn.connect()
@@ -171,7 +172,7 @@ class DataGenerator:
             
             for order_date in date_list:
                 print(f"Start generating sales data for {order_date} and save into csv files.")
-                table_names_and_paths = self.generate_1_day_sales_data(n_orders, order_date, n_processes)
+                table_names_and_paths = self.generate_1_day_sales_data(n_orders, order_date)
                 print("Finish generating data.")
                 for table, csv_path in table_names_and_paths.items():
                     load_query = f"""
